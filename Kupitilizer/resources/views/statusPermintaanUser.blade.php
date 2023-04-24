7939d137d117a5fe1948aca721ac1b1fc95976a4
Rahmat Azrima <rahmat_az@mhs.unsyiah.ac.id>
2023-04-25 02:06:09 +0700

    feat: test

--- Kupitilizer/resources/views/statusPermintaanUser.blade.php
+++ Kupitilizer/resources/views/statusPermintaanUser.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     <p class="text-center text-3xl font-bold m-10">To Be Added Soon</p>
     <img src="{{asset('images/mt.jpeg')}}" class="w-full p-32 pt-0"></img>
+    <h1>TEST</h1>
 @endsection
